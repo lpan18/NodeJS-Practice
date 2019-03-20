df2426bbcf41bd619196d75582fffd091e6cbc71
Lei <sophie.leipan@gmail.com>
2019-03-19 21:56:34 -0700

    add and delete user

--- Practice2/routes/users.js
+++ Practice2/routes/users.js
@@ -4,10 +4,26 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/userlist', function(req, res, next) {
   var db = req.db;
-  console.log(db);
   var collection = db.get('userlist');
-  collection.find({},{},function(e,docs){
-    res.json(docs);
+  collection.find({},function(err,result){
+    res.json(result);
+  });
+});
+
+router.post('/adduser', function(req, res){
+  var db = req.db;
+  var collection = db.get('userlist');
+  collection.insert(req.body, function(err, result){
+    res.send((err==null)? {msg:''} : {msg:err});
+  });
+});
+
+router.delete('/deleteuser/:id', function(req, res){
+  var db = req.db;
+  var collection = db.get('userlist');
+  var userToDelete = req.params.id;
+  collection.remove({'_id':userToDelete}, function(err){
+    res.send((err==null)? {msg:''} : {msg:err});
   });
 });
 
